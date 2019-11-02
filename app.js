8d63392f0343a40958e806443ef593b8ae18ea81
Jiahua Zhou <vince.zhou16@gmail.com>
2019-11-01 21:19:01 -0400

    Initial setup

---
+++ app.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+const fs = require('fs');
+const csv = require('csv-parser');
+const { Client, Pool } = require('pg')
+var copyFrom = require('pg-copy-streams').from;
+var pool = new Pool();
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  multiStatements: true
+});
+
+client.connect()
+
+const create_database = 'CREATE DATABASE schoolDB;'
+const use_database = 'USE schoolDB'
+
+/*
+fs.readFile('./sql/report.sql', 'utf8', function (err, data) {
+  if (err) throw err;
+
+  var sql = data.split(';')
+      .filter((element) => {
+          return element.length != 0
+      })
+      .map((element) => {
+          if (element.length != 0)
+              return element.replace(/\r?\n|\r/g, " ");
+      });
+
+  for (var iterator in sql) {
+      client.query(sql[iterator], (err, rows) => {
+          if (err) throw err;
+          else console.log(iterator);
+      });
+  }
+});
+
+pool.connect(function(err, client, done) {
+  var stream = client.query(copyFrom('COPY courses FROM STDIN'));
+  var fileStream = fs.createReadStream('./data/courses.csv')
+  fileStream.on('error', done);
+  stream.on('error', done);
+  stream.on('end', done);
+  fileStream.pipe(stream);
+});
+*/
+
+
+
---
+++ sql/report.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS `schoolDB`;
+USE `schoolDB`;
+
+CREATE TABLE IF NOT EXISTS `courses` (
+  `id` int NOT NULL,
+  `name` varchar(100),
+  `teacher` varchar(100),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `marks` (
+  `test_id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `mark` int,
+  PRIMARY KEY (`test_id`, `student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `students` (
+  `id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `tests` (
+  `id` int NOT NULL,
+  `course_id` int NOT NULL,
+  `weight` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
